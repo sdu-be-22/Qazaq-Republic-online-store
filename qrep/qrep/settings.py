f35e68db3c8bd7ee030f70ef7adcb416a58c7861
Gvzum <diasgazim@icloud.com>
2022-05-10 03:43:29 +0600

    Last!

--- qrep/qrep/settings.py
+++ qrep/qrep/settings.py
@@ -158,8 +158,7 @@ ACCOUNT_LOGOUT_REDIRECT = '/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
